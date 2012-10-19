78a484ec779135bad4ee5cf461349796549026f3
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-10-20 00:37:42 +0200

    Initial commit.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

---
+++ confutils/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# This code is distributed under the LGPLv3+ license.
+# Copyright (c) 2012 Raphaël Barrois
+
+__author__ = u"Raphaël Barrois <raphael.barrois+confutils@polytechnique.org>"
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This code is distributed under the LGPLv3+ license.
+# Copyright (c) 2012 Raphaël Barrois
+
+from setuptools import setup, find_packages
+import os
+import re
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(package_name):
+    version_re = re.compile(r"^__version__ = [\"']([\w_.-]+)[\"']$")
+    package_components = package_name.split('.')
+    path_components = package_components + ['__init__.py']
+    with open(os.path.join(root_dir, *path_components)) as f:
+        for line in f:
+            match = version_re.match(line[:-1])
+            if match:
+                return match.groups()[0]
+    return '0.1.0'
+
+
+PACKAGE = 'confutils'
+
+
+setup(
+    name="confutils",
+    version=get_version(PACKAGE),
+    author="Raphaël Barrois",
+    author_email="raphael.barrois+confutils@polytechnique.org",
+    description="Advanced configuration file utilities.",
+    license="LGPLv3+",
+    keywords=['project', 'library', 'template'],
+    url="http://github.com/rbarrois/confutils",
+    download_url="http://pypi.python.org/pypi/confutils/",
+    packages=find_packages(),
+    setup_requires=[
+        'distribute',
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='tests',
+)
+
