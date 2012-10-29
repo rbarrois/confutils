567cf19f772687fd09319e8db8c5fe417318583c
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-10-29 02:06:36 +0100

    Announce Py3 compatibility.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- setup.py
+++ setup.py
@@ -48,6 +48,9 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     test_suite='tests',
 )
