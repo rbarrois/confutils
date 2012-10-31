b7ac5c15a23314bce19da59a6f0c40485b4aa914
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-10-31 20:31:47 +0100

    Version bump to v0.3.3
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- confutils/__init__.py
+++ confutils/__init__.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 
 __author__ = "Raphaël Barrois <raphael.barrois+confutils@polytechnique.org>"
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 from .configfile import ConfigFile, ConfigLine, Parser
 from .configfile import ConfigError, ConfigReadingError, ConfigWritingError
